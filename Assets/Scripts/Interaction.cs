3a6a490a73c3565c1dc453c353349b4bccd45957
John Choi <johnchoi313@gmail.com>
2025-01-23 12:17:52 -0500

    example of ui + functions

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,33 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interaction : MonoBehaviour
 {
+    public TMP_Text thistext;
+
     public GameObject cube;
     public int integer = 1;
     public float decimalNumber = 1.0f;
     public bool yes = true;
     public bool no = false;
+
+    public void DoThisIfButtonPress(float degrees) 
+    {
+        cube.transform.Rotate(0,degrees,0);
+        thistext.text = "numba = " + Random.Range(0,100);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
+        thistext.text = "billy";
     }
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            DoThisIfButtonPress(5.0f);
+        }
+
         float dt = Time.deltaTime;
         //cube.transform.Rotate(0,decimalNumber * dt,0);
-        
         if(Input.GetKey(KeyCode.A))
         {
             cube.transform.Translate(-decimalNumber * dt,0,0);   
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.S))
         {
             cube.transform.Translate(0,integer,0);   
         }
-
-
     }
 }
